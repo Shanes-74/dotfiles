1ed2636f9e0f1f4b59c38cf445d4ca257f489692
Shanes-74 <gabbaggi@gmail.com>
2025-11-30 02:57:01 -0300

    Automação para instalação

---
+++ instalacao.sh
@@ -0,0 +1,1 @@
+sudo pacman -Syyu --noconfirm ttf-dejavu noto-fonts-emoji ttf-liberation noto-fonts noto-fonts-cjk gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libdvdread libdvdnav libdvdcss a52dec libde265 x264 x265 xvidcore xdg-desktop-portal xdg-desktop-portal-hyprland intel-ucode dolphin
