eaa7538cd5bca7e0e4088c88dfc1a71a331fe70f
Shanes-74 <gabbaggi@gmail.com>
2025-11-30 05:45:43 -0300

    Atualização no script de instalação

--- instalacao.sh
+++ instalacao.sh
@@ -1,7 +1,7 @@
 #!\bin\bash
 
-sudo pacman -Syyu --noconfirm ttf-dejavu noto-fonts-emoji ttf-liberation noto-fonts noto-fonts-cjk gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libdvdread libdvdnav libdvdcss a52dec libde265 x264 x265 xvidcore xdg-desktop-portal xdg-desktop-portal-hyprland intel-ucode
-sudo pacman -S --noconfirm --needed dolphin stow waybar rofi
+sudo pacman -Syyu --noconfirm git base-devel ttf-dejavu noto-fonts-emoji ttf-liberation noto-fonts noto-fonts-cjk gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libdvdread libdvdnav libdvdcss a52dec libde265 x264 x265 xvidcore xdg-desktop-portal xdg-desktop-portal-hyprland intel-ucode
+sudo pacman -S --noconfirm --needed dolphin stow waybar rofi swww
 
 cd ~/dotfiles
 stow hyprland
