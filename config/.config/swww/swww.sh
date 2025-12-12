52c951665c64c3b7c7bf2fcdad57523984e5bce6
Shanes-74 <gabbaggi@gmail.com>
2025-12-12 04:41:45 -0300

    Mudança na troca de wallpaper

--- config/.config/swww/swww.sh
+++ config/.config/swww/swww.sh
@@ -1,5 +1,7 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 #inicie o swww
 WALLPAPERS_DIR=~/Imagens/Wallpapers
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
-swww img "$WALLPAPER"
+
+# Mude para wave e defina 60 frames por segundo
+swww img "$WALLPAPER" --transition-type grow --transition-fps 144
