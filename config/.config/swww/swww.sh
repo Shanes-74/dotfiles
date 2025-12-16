45899a2a66f232b0b243505b268addc652901df0
Shanes-74 <gabbaggi@gmail.com>
2025-12-16 03:27:51 -0300

    Mudança da pasta de wallpaper do swww

--- config/.config/swww/swww.sh
+++ config/.config/swww/swww.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #inicie o swww
-WALLPAPERS_DIR=~/Imagens/Wallpapers
+WALLPAPERS_DIR=~/Imagens/Wallpapers-catppuccin
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
 
 # Mude para wave e defina 60 frames por segundo
