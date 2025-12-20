11898e8242b6a842c088889407dfb400177cd687
Shanes-74 <gabbaggi@gmail.com>
2025-12-20 08:48:45 -0300

    Correção no swww

--- config/.config/swww/swww.sh
+++ config/.config/swww/swww.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #inicie o swww
-WALLPAPERS_DIR=~/Imagens/Wallpapers-catppuccin
+WALLPAPERS_DIR=~/Imagens/Wallpapers
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
 
 # Mude para wave e defina 60 frames por segundo
