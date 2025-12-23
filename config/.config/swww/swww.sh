3ec88331adbaba1943314a55205bf4868f82d128
Shanes-74 <gabbaggi@gmail.com>
2025-12-23 01:08:00 -0300

    Remoção do modulo de alterar papel de parede na waybar

--- config/.config/swww/swww.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-#inicie o swww
-WALLPAPERS_DIR=~/Imagens/Wallpapers
-WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
-
-# Mude para wave e defina 60 frames por segundo
-swww img "$WALLPAPER" --transition-type grow --transition-fps 144
